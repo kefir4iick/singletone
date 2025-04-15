0231c6fc5bcc6093ea2923d2a3f99f7972a8c105
kefir4iick <lizardm17@icloud.com>
2025-04-15 23:58:00 +0300

    add tests

---
+++ goodsingletone/test/cartest.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Threading;
+using Xunit;
+using GoodSingleton;
+
+namespace GoodSingleton.Tests
+{
+    public class CarTests
+    {
+        [Fact]
+        public void DemonstrateRaceCondition()
+        {
+            bool race = false; 
+
+            for (int i = 0; i < 100; i++) 
+            {
+                if (RunRaceConditionTest())
+                {
+                    Console.WriteLine("there is race condition");
+                    race = true;
+                    break; 
+                }
+                Console.WriteLine();
+            }
+
+            if (!race)
+            {
+                Console.WriteLine("there is no race condition");
+            }
+        }
+
+        private bool RunRaceConditionTest()
+        {
+            Car.Reset();
+
+            string first = null;
+            string second = null;
+
+            var thread1 = new Thread(() => 
+                first = Car.GetInstance("corvette").Name);
+                
+            var thread2 = new Thread(() => 
+                second = Car.GetInstance("porsche").Name);
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            return first != second;
+        }
+    }
+}
